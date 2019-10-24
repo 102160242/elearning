bb86a602f58b5fd7f56fb4f96d53bc0a774d2164
baochanh18 <44717938+baochanh18@users.noreply.github.com>
2019-10-24 21:19:55 +0700

    login page
    xong auth.js
    xong login.js
    chỉnh sửa css của auth và login

--- src/app/auth/Auth.js
+++ src/app/auth/Auth.js
@@ -12,32 +12,20 @@ class Auth extends React.Component {
         this.state = {  };
     }
     render() {
+
         return (
             <>
-                <div className="container-fluid">
-                    <div className="row" >
-                        <div className="col-md-4 slide p-5" style={{ backgroundImage: `url(${Background})` , color : 'white'}}>
-                            <div className="logo">
-                                <Link to="/" className="">
-                                    <b>Home</b>
-                                </Link>
-                            </div>
-                            <div className="middle_part">
-                                asdasdasdasd
-                            </div>
-                            <div className="info">
-                                asdasdasdasd
-                            </div>
-                            
-                        </div>
-                        <div className="col-md-6">
+                <div className="container-fluid ">
+                    <div className="row slide d-flex justify-content-center align-items-center" style={{backgroundImage: `url(${Background})` , color : '#595d6e'}}>
+                        <div className="col-xl-4 mx-auto ">    
                             <Switch>
                                 <Redirect from="/auth" exact={true} to="/auth/login" />
                                 <Route exact path="/auth/login" component={Login} />
                                 <Route exact path="/auth/forgotPassword" component={ForgotPassword} />
                                 <Route exact path="/auth/registration" component={Registration} />
                             </Switch>
-                        </div>
+                        </div >
+                            
                     </div>
                 </div>
             </>
--- src/app/auth/Login.js
+++ src/app/auth/Login.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link ,Route, Switch, Redirect} from 'react-router-dom';
+import './Login.css'
 
 class Login extends React.Component {
     constructor(props) {
@@ -7,7 +9,45 @@ class Login extends React.Component {
     }
     render() {
         return (
-            <div>Login Page</div>    
+            <>
+            <div className="container-fluid ">
+                <div className="d-flex justify-content-center p-2" >
+                    <Link to="/">
+                        <h1>
+                            <i class="fas fa-home"></i>
+                        </h1>
+                    </Link>
+                    
+                </div>
+                <div className="d-flex justify-content-center p-2">
+                    <h3>
+                        
+                        Sign In
+                    </h3>
+                </div>
+                <form >
+                    <div class="form-group">
+                    <input type="email" class="form-control border border-0 input_place" id="email" placeholder="Email" name="email" />
+                    </div>
+                    <div class="form-group">
+                    <input type="password" class="form-control border border-0 input_place" id="pwd" placeholder="Password" name="pswd"/>
+                    </div>
+                    <div class="form-group form-check d-flex justify-content-between ">
+                    <label class="form-check-label ">
+                        <input class="form-check-input" type="checkbox" name="remember"/> Remember me
+                    </label>
+                    <Link to="/auth/forgotPassword">Forget Password?</Link>
+                    </div>
+                    <div className="d-flex justify-content-center ">
+                        <button type="submit" class="btn btn-primary btn-lg btn">Sign In</button>
+                    </div>
+                    <div className="d-flex justify-content-center p-4">
+                        Don't have account yet?  
+                        <Link to="/auth/registration" >Sign Up</Link>
+                    </div>
+                </form>
+            </div>
+             </>
         );
     }
 }
