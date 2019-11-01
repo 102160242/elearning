09ac021277402de109040b4f4b6e2fd17fe9f4bd
Minhchien0204 <48354680+Minhchien0204@users.noreply.github.com>
2019-11-01 21:34:25 +0700

    newsfeed
    làm giao diện

--- src/app/pages/NewsFeed.js
+++ src/app/pages/NewsFeed.js
@@ -7,7 +7,66 @@ class NewsFeed extends React.Component {
     }
     render() {
         return (
-            <div>News Feed Page</div>
+            <div>
+                <div className="container mt-6">
+                   <div className="information">
+                        <div className="row">
+                            <div className="col-auto">
+                                <div className="anh mt-5">
+                                    <img src="https://www.thepaintedturtle.org/sites/main/files/main-images/camera_lense_0.jpeg" alt="image" class="img-responsive" width="60" height="60"></img>  
+                                </div>
+                            </div>
+                            <div className="col mb-3  ml-md-n2">
+                               <div className="mt-5">
+                                   <a>David Smit</a>
+                               </div>
+                               <div className="mt-2">
+                                <i class="far fa-envelope mt-2"></i> david.s@loop.com
+                               </div>
+                            </div>
+                        </div>
+                   </div>
+                   <hr></hr>
+                </div>
+                <div className="container mt-4">
+                    <div className="row">
+                        <div className="col-md-8">
+                            <p>There is no activity to show</p>
+                        </div>
+                        <div className="col-md-4">
+                            <table className="table-responsive">
+                                <table className="table table-striped border">
+                                    <tbody>
+                                        <tr>
+                                            <td>Joined</td>
+                                            <td>2019-10-23 14:42:23 UTC</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Learnt</td>
+                                            <td>
+                                                <a href="#" class="badge badge-pill badge-primary">Words <span class="badge badge-light">9</span> </a>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>Following</td>
+                                            <td>
+                                                <a href="#" class="badge badge-pill badge-success">Users <span class="badge badge-light">5</span> </a>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>Followers</td>
+                                            <td>
+                                                <a href="#" class="badge badge-pill badge-info">Users <span class="badge badge-light">13</span> </a>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <hr></hr>
+            </div>
         );
     }
 }
