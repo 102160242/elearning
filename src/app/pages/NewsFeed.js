cb8a838556efa0b9476bcfd135b897d209855070
Minhchien0204 <48354680+Minhchien0204@users.noreply.github.com>
2019-11-02 12:44:01 +0700

    Sửa profile và newfeed
    lấy thong tin user

--- src/app/pages/NewsFeed.js
+++ src/app/pages/NewsFeed.js
@@ -1,74 +1,105 @@
 import React from 'react';
+import {Redirect} from 'react-router-dom';
+import {connect} from 'react-redux';
 
 class NewsFeed extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {  };
+        this.state = { 
+            name:"",
+            email:"",
+         };
+         this.handleChange = this.handleChange.bind(this);
+         this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleChange(e) {
+        //console.log(this.state.email);
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    }
+    handleSubmit(e)
+    {
+        e.preventDefault();
     }
     render() {
-        return (
-            <div>
-                <div className="container mt-6">
-                   <div className="information">
-                        <div className="row">
-                            <div className="col-auto">
-                                <div className="anh mt-5">
-                                    <img src="https://www.thepaintedturtle.org/sites/main/files/main-images/camera_lense_0.jpeg" alt="image" class="img-responsive" width="60" height="60"></img>  
+        if(!this.props.isLoggedIn) {
+            return (
+                <Redirect to={process.env.REACT_APP_ROOT_URL}></Redirect>
+            )
+        }else{
+            return (
+                <div>
+                    <div className="container mt-6">
+                       <div className="information">
+                            <div className="row">
+                                <div className="col-auto">
+                                    <div className="anh mt-5">
+                                        <img src="https://www.thepaintedturtle.org/sites/main/files/main-images/camera_lense_0.jpeg" alt="image" class="img-responsive" width="60" height="60"></img>  
+                                    </div>
+                                </div>
+                                <div className="col mb-3  ml-md-n2">
+                                   <div className="mt-5">
+                                   <p>{this.props.currentUser.name}</p>
+                                   </div>
+                                   <div className="mt-2">
+                                    <p><i class="far fa-envelope mt-2 mr-2"></i>{this.props.currentUser.email}</p>
+                                   </div>
                                 </div>
                             </div>
-                            <div className="col mb-3  ml-md-n2">
-                               <div className="mt-5">
-                                   <a>David Smit</a>
-                               </div>
-                               <div className="mt-2">
-                                <i class="far fa-envelope mt-2"></i> david.s@loop.com
-                               </div>
+                       </div>
+                       <hr></hr>
+                    </div>
+                    <div className="container mt-4">
+                        <div className="row">
+                            <div className="col-md-8">
+                                <p>There is no activity to show</p>
+                            </div>
+                            <div className="col-md-4">
+                                <table className="table-responsive">
+                                    <table className="table table-striped border">
+                                        <tbody>
+                                            <tr>
+                                                <td>Joined</td>
+                                                <td>2019-10-23 14:42:23 UTC</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Learnt</td>
+                                                <td>
+                                                    <a href="#" class="badge badge-pill badge-primary">Words <span class="badge badge-light">9</span> </a>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td>Following</td>
+                                                <td>
+                                                    <a href="#" class="badge badge-pill badge-success">Users <span class="badge badge-light">5</span> </a>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td>Followers</td>
+                                                <td>
+                                                    <a href="#" class="badge badge-pill badge-info">Users <span class="badge badge-light">13</span> </a>
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </table>
                             </div>
                         </div>
-                   </div>
-                   <hr></hr>
-                </div>
-                <div className="container mt-4">
-                    <div className="row">
-                        <div className="col-md-8">
-                            <p>There is no activity to show</p>
-                        </div>
-                        <div className="col-md-4">
-                            <table className="table-responsive">
-                                <table className="table table-striped border">
-                                    <tbody>
-                                        <tr>
-                                            <td>Joined</td>
-                                            <td>2019-10-23 14:42:23 UTC</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Learnt</td>
-                                            <td>
-                                                <a href="#" class="badge badge-pill badge-primary">Words <span class="badge badge-light">9</span> </a>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td>Following</td>
-                                            <td>
-                                                <a href="#" class="badge badge-pill badge-success">Users <span class="badge badge-light">5</span> </a>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td>Followers</td>
-                                            <td>
-                                                <a href="#" class="badge badge-pill badge-info">Users <span class="badge badge-light">13</span> </a>
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </table>
-                        </div>
                     </div>
+                    <hr></hr>
                 </div>
-                <hr></hr>
-            </div>
-        );
+            );
+        }
+       
+    }
+}
+const mapStateToProps = (state /*, ownProps*/) => {
+    //console.log(state);
+    return {
+      currentUser: state.auth.currentUser,
+      isLoggedIn: state.auth.isLoggedIn
     }
 }
 
-export default NewsFeed;
+export default connect(mapStateToProps, null) (NewsFeed);
--- src/app/pages/Profile.js
+++ src/app/pages/Profile.js
@@ -1,71 +1,111 @@
 import React from 'react';
+import {Redirect} from 'react-router-dom';
+import {connect} from 'react-redux';
+
 
 class Profile extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {  };
+        this.state = { 
+            email: "",
+            name: "",
+            };
+            this.handleChange = this.handleChange.bind(this);
+            this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleChange(e) {
+        //console.log(this.state.email);
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    }
+    handleSubmit(e)
+    {
+        e.preventDefault();
     }
     render() {
-        return (
-            <div className="container">
-                <h3 className="mt-3">Profile</h3>
-                <div className="row">
-                    <div className="col-md">
-                        <ul class="nav nav-pills flex-column pt-2" >
-                            <li className="nav-item"><a href="#personal" aria-controls="personal" role="tab" data-toggle="tab" className="nav-link active">Personal Information</a></li>
-                            <li className="nav-item"><a href="#password" aria-controls="password" role="tab" data-toggle="tab" className="nav-link">Change Password</a></li>
-                            {/* <li className="nav-item"><a href="#following" aria-controls="following" role="tab" data-toggle="tab" className="nav-link">Following</a></li>
-                                <li className="nav-item"><a href="#followers" aria-controls="followers" role="tab" data-toggle="tab" className="nav-link">Followers</a></li> */}
-                        </ul>
-                    </div>
-                    <div className="col-md-8 pt-2">
-                        <div className="group-tabs">
-                            <div class="tab-content col-md-12">
-                            <div role="tabpanel" class="tab-pane in active" id="personal">
-                                    <p>Personal Information:</p>
-                                    User's Information...
-                                </div>
-                                <div role="tabpanel" class="tab-pane" id="password">
-                                    <p>Change Password:</p>
+        if(!this.props.isLoggedIn) {
+            return (
+                <Redirect to={process.env.REACT_APP_ROOT_URL}></Redirect>
+            )
+        }
+        else
+        {
+            return (
+                <div className="container">
+                    <h3 className="mt-3">Profile</h3>
+                    <div className="row">
+                        <div className="col-md">
+                            <ul class="nav flex-column pt-2" >
+                                <li className="nav-item"><a href="#personal" aria-controls="personal" role="tab" data-toggle="tab" className="nav-link active">Personal Information</a></li>
+                                <li className="nav-item"><a href="#password" aria-controls="password" role="tab" data-toggle="tab" className="nav-link">Change Password</a></li>
+                                {/* <li className="nav-item"><a href="#following" aria-controls="following" role="tab" data-toggle="tab" className="nav-link">Following</a></li>
+                                    <li className="nav-item"><a href="#followers" aria-controls="followers" role="tab" data-toggle="tab" className="nav-link">Followers</a></li> */}
+                            </ul>
+                        </div>
+                        <div className="col-md-8 pt-2">
+                            <div className="group-tabs">
+                                <div class="tab-content col-md-12">
+                                <div role="tabpanel" class="tab-pane in active" id="personal">
+                                        <p>Personal Information:</p>
+                                        User's Information...
+                                    </div>
+                                    <div role="tabpanel" class="tab-pane" id="password">
+                                        <p>Change Password:</p>
+    
+                                        <form onSubmit={this.handleSubmit}>
+                                            <div className="form-group">
+                                                <label for="name">Full Name</label>
+                                                <input type="text" className="form-control" placeholder="Full Name" id="name" value={this.state.name === "" ? this.props.currentUser.name : this.state.name} onChange={this.handleChange}></input>
+                                            </div>
+                                            <div className="form-group danger">
+                                                <label for="email">Email</label>
+                                                <input type="email" className="form-control" id="email" value={this.props.currentUser.email} disabled></input>
+                                            </div>
 
-                                    <form>
-                                        <div className="form-group">
-                                            <label for="name">Full Name</label>
-                                            <input type="text" className="form-control" placeholder="Full Name" id="name"></input>
-                                        </div>
-                                        <div className="form-group danger">
-                                            <label for="email">Email</label>
-                                            <input type="email" className="form-control" id="email" disabled></input>
-                                        </div>
-                                        <div className="form-group danger">
-                                            <label for="pw">Password</label>
-                                            <input type="password" className="form-control" id="pw"></input>
-                                        </div>
-                                        <div className="form-group danger">
-                                            <label for="dpw">Password confirmation</label>
-                                            <input type="password" className="form-control" id="dpw" placeholder="Type your password once again"></input>
-                                        </div>
-                                        <div className="form-group danger">
-                                            <label for="cpw">Current password</label>
-                                            <input type="password" className="form-control" id="pw" placeholder="Your current password"></input>
-                                        </div>
-                                        <button type="submit" className="btn btn-outline-primary mr-3">Update</button>
-                                        <button type="clear" className="btn btn-outline-secondary">Clear</button>
-                                    </form>
+                                            <div className="form-group danger">
+                                                <label for="cpw">Current password</label>
+                                                <input type="password" className="form-control" id="pw" placeholder="Your current password"></input>
+                                            </div>
+
+                                            <div className="form-group danger">
+                                                <label for="pw">Password</label>
+                                                <input type="password" className="form-control" id="pw"></input>
+                                            </div>
+
+                                            <div className="form-group danger">
+                                                <label for="dpw">Password confirmation</label>
+                                                <input type="password" className="form-control" id="dpw" placeholder="Type your password once again"></input>
+                                            </div>
+                                            
+                                            <button type="submit" className="btn btn-outline-primary mr-3">Update</button>
+                                            <button type="clear" className="btn btn-outline-secondary">Clear</button>
+                                        </form>
+                                    </div>
+                                    {/* <div role="tabpanel" class="tab-pane" id="followers">
+                                        danh sach followers
+                                     </div>
+                                    <div role="tabpanel" class="tab-pane" id="following">
+                                        danh sach following
+                                     </div> */}
                                 </div>
-                                {/* <div role="tabpanel" class="tab-pane" id="followers">
-                                    danh sach followers
-                                 </div>
-                                <div role="tabpanel" class="tab-pane" id="following">
-                                    danh sach following
-                                 </div> */}
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        );
+            );
+        }
+       
     }
 }
 
-export default Profile;
+const mapStateToProps = (state /*, ownProps*/) => {
+    //console.log(state);
+    return {
+      currentUser: state.auth.currentUser,
+      isLoggedIn: state.auth.isLoggedIn
+    }
+}
+
+export default connect(mapStateToProps, null) (Profile);
+//export default Profile;
